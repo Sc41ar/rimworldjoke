80c9ea4e1d94a160f0381646f3dbeb3b95690d0f
Nikita <mrniki2396@gmail.com>
2023-08-03 23:31:55 +0700

    First commit

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "12356" );
+        GUI gui = new GUI();
+    }
+
+
+}
---
+++ src/main/java/GUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI(){
+        this.setBounds(0, 0, 500, 500);
+        this.setVisible(true);
+    }
+
+}
