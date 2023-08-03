c4755dc4ebd180f0cc7abb33d9585cd1bc531439
Nikita <mrniki2396@gmail.com>
2023-08-04 00:03:50 +0700

    second commit)))

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "12356" );
+        System.out.println( "1256" );
         GUI gui = new GUI();
+        gui.init();
+
     }
 
 
--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,10 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
+
+    private JPanel canvas;
+    private JSlider populationSlider;
+    private JLabel populationLabel;
+    private JLabel riceCount;
+    private JLabel potatoCount;
+    private JLabel cornCount;
 
     public GUI(){
         this.setBounds(0, 0, 500, 500);
         this.setVisible(true);
     }
+
+    public void init()
+    {
+        canvas = new JPanel();
+        populationSlider = new JSlider();
+        populationLabel = new JLabel("population: 0");
+        cornCount = new JLabel("corn count: 0");
+        riceCount = new JLabel("rice count: 0");
+        potatoCount = new JLabel("potato count: 0");
+        populationSlider.setPreferredSize(new Dimension(200, 50));
+        populationLabel.setPreferredSize(new Dimension(100, 20));
+        canvas.setLayout(new BorderLayout());
+        canvas.add(populationLabel, BorderLayout.LINE_START);
+        canvas.add(populationSlider, BorderLayout.LINE_START);
+        canvas.add(riceCount,BorderLayout.LINE_END);
+        canvas.add(potatoCount, BorderLayout.LINE_END);
+        canvas.add(cornCount, BorderLayout.LINE_END);
+        canvas.setPreferredSize(new Dimension(100,100));
+        this.add(canvas);
+        this.repaint();
+    }
+
 
 }
